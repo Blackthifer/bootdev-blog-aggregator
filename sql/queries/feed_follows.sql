a79260412194fbe82ff86cb1b28d1030f2e4a988
Blackthifer <hessel96@msn.com>
2025-06-29 12:22:12 +0200

    adds queries to create and lookup feed_follows entries

---
+++ sql/queries/feed_follows.sql
@@ -0,0 +1,16 @@
+-- name: CreateFeedFollow :one
+WITH feed_follows_row AS (
+    INSERT INTO feed_follows(id, created_at, updated_at, user_id, feed_id)
+    VALUES ($1, $2, $2, $3, $4)
+    RETURNING *
+) SELECT feed_follows_row.*, users.user_name, feeds.feed_name
+FROM feed_follows_row
+INNER JOIN users ON feed_follows_row.user_id = users.id
+INNER JOIN feeds ON feed_follows_row.feed_id = feeds.id;
+
+-- name: GetFeedFollowsForUser :many
+SELECT feed_follows.*, users.user_name, feeds.feed_name
+FROM feed_follows
+INNER JOIN users ON feed_follows.user_id = users.id
+INNER JOIN feeds ON feed_follows.feed_id = feeds.id
+WHERE feed_follows.user_id = $1;
